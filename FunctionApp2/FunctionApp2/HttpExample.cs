ffc570f91bf4f6654db8715ae9d650cbd0c36431
Jhonatan <jhont.escobar17@gmail.com>
2024-09-26 11:29:23 -0500

    Add example post azure function

--- FunctionApp2/FunctionApp2/HttpExample.cs
+++ FunctionApp2/FunctionApp2/HttpExample.cs
@@ -1,7 +1,9 @@
+using FunctionApp2.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace FunctionApp2
 {
@@ -15,10 +17,21 @@ namespace FunctionApp2
         }
 
         [Function("HttpExample")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            _logger.LogInformation("Processing POST request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            var data = JsonConvert.DeserializeObject<TaskModel>(requestBody);
+
+            if (data == null || string.IsNullOrEmpty(data.Name))
+            {
+                return new BadRequestObjectResult("Invalid input");
+            }
+
+            return new OkObjectResult($"Task {data.Name} created successfully.");
         }
     }
 }
